13b1a75f5da9ba2ef5cd575c54e4675cc2a216c9
Lena Tauchner <lena.tauchner@rubicon.eu>
2024-02-24 07:48:05 +0100

    - add to vault database objects

--- Services/Vault/Vault.Service.Domain/Models/PersistentVault.cs
+++ Services/Vault/Vault.Service.Domain/Models/PersistentVault.cs
@@ -1,7 +1,13 @@
 using DeadmanSwitchFailed.Common.Domain;
+using System;
 
 namespace DeadmanSwitchFailed.Services.Vault.Service.Domain.Models;
 
 public class PersistentVault : Persistent<Vault>
 {
+  public string DisplayName { get; set; }
+  public string Description { get; set; }
+  public string AccessToken { get; set; }
+  public string PasswordHash { get; set; }
+  public Guid OwningUserId { get; set; }
 }
--- Services/Vault/Vault.Service.Domain/Models/Vault.cs
+++ Services/Vault/Vault.Service.Domain/Models/Vault.cs
@@ -1,9 +1,20 @@
 using DeadmanSwitchFailed.Common.Domain;
+using System;
 
 namespace DeadmanSwitchFailed.Services.Vault.Service.Domain.Models;
 
 public class Vault : Aggregate<PersistentVault>
 {
-  public override IAggregate FromData(IPersistent data) =>
-    throw new System.NotImplementedException();
+  public string DisplayName { get; set; }
+  public string Description { get; set; }
+  public string AccessToken { get; set; }
+  public string PasswordHash { get; set; }
+  public Guid OwningUserId { get; set; }
+
+  public override IAggregate FromData(IPersistent data)
+  {
+    Data = data as PersistentVault;
+
+    return this;
+  }
 }
